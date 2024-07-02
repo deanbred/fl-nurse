a9fdd7628bda5f00472fe75082bfb446198b103d
deanbred <deanbred@gmail.com>
2024-07-01 22:54:14 -0400

    imgs

--- .next/static/mGfoIwvV89dtSnhfidD3W/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/487-91e111ae2cea50a5.js","static/css/aaeb027da46ad39d.css","static/chunks/pages/index-cef86775f8bde72d.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- pages/index.js
+++ pages/index.js
@@ -6,19 +6,43 @@ import { SlideshowLightbox, initLightboxJS } from "lightbox.js-react"
 import React, { useEffect, useState } from "react"
 import Ticker from "@/components/Ticker"
 
+/* import fs from "fs"
+const images = fs.readdirSync("./public/images").map((file) => ({
+  src: `/images/${file}`,
+  alt: `Image ${file.slice(3, -4)}`,
+})) */
+
 const images = [
-  { src: "/images/img01.png", alt: "Image 1" },
-  { src: "/images/img02.png", alt: "Image 2" },
-  { src: "/images/img03.png", alt: "Image 3" },
-  { src: "/images/img04.png", alt: "Image 4" },
-  { src: "/images/img05.png", alt: "Image 5" },
-  { src: "/images/img06.png", alt: "Image 6" },
-  { src: "/images/img07.png", alt: "Image 7" },
-  { src: "/images/img08.png", alt: "Image 8" },
-  { src: "/images/img09.png", alt: "Image 9" },
-  { src: "/images/img10.png", alt: "Image 10" },
-  { src: "/images/img11.png", alt: "Image 11" },
-  { src: "/images/img12.png", alt: "Image 12" },
+  { src: "/images/img001.jpg", alt: "Image 1" },
+  { src: "/images/img002.jpg", alt: "Image 2" },
+  { src: "/images/img003.jpg", alt: "Image 3" },
+  { src: "/images/img004.jpg", alt: "Image 4" },
+  { src: "/images/img005.jpg", alt: "Image 5" },
+  { src: "/images/img006.jpg", alt: "Image 6" },
+  { src: "/images/img007.jpg", alt: "Image 7" },
+  { src: "/images/img008.jpg", alt: "Image 8" },
+  { src: "/images/img009.jpg", alt: "Image 9" },
+  { src: "/images/img010.jpg", alt: "Image 10" },
+  { src: "/images/img011.jpg", alt: "Image 11" },
+  { src: "/images/img012.jpg", alt: "Image 12" },
+  { src: "/images/img013.jpg", alt: "Image 13" },
+  { src: "/images/img014.jpg", alt: "Image 14" },
+  { src: "/images/img015.jpg", alt: "Image 15" },
+  { src: "/images/img016.jpg", alt: "Image 16" },
+  { src: "/images/img017.jpg", alt: "Image 17" },
+  { src: "/images/img018.jpg", alt: "Image 18" },
+  { src: "/images/img019.jpg", alt: "Image 19" },
+  { src: "/images/img020.jpg", alt: "Image 20" },
+  { src: "/images/img021.jpg", alt: "Image 21" },
+  { src: "/images/img022.jpg", alt: "Image 22" },
+  { src: "/images/img023.jpg", alt: "Image 23" },
+  { src: "/images/img024.jpg", alt: "Image 24" },
+  { src: "/images/img025.jpg", alt: "Image 25" },
+  { src: "/images/img026.jpg", alt: "Image 26" },
+  { src: "/images/img027.jpg", alt: "Image 27" },
+  { src: "/images/img028.jpg", alt: "Image 28" },
+  { src: "/images/img029.jpg", alt: "Image 29" },
+  { src: "/images/img030.jpg", alt: "Image 30" },
 ]
 
 export default function Home() {
