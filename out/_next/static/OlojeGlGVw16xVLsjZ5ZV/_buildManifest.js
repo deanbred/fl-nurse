64ddca2357ba569a97df60f9d7a74a138168b041
deanbred <deanbred@gmail.com>
2024-07-02 01:46:33 -0400

    bg

--- out/_next/static/OlojeGlGVw16xVLsjZ5ZV/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/487-91e111ae2cea50a5.js","static/css/aaeb027da46ad39d.css","static/chunks/pages/index-f1469b4aa84c6ddd.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,6 @@ import { SocialIcon } from "react-social-icons"
 import "lightbox.js-react/dist/index.css"
 import { SlideshowLightbox, initLightboxJS } from "lightbox.js-react"
 import React, { useEffect, useState } from "react"
-import Ticker from "@/components/Ticker"
 
 /* import fs from "fs"
 const images = fs.readdirSync("./public/images").map((file) => ({
@@ -46,8 +45,6 @@ const images = [
 ]
 
 export default function Home() {
-  const [copiedText, setCopiedText] = useState("G6KJREZ5TxiLUU6JwYcghbqDa91oU6D6oapM68P6N5Ve")
-
   useEffect(() => {
     initLightboxJS("DF8A-D560-5C71-E585", "Individual")
   }, [])
@@ -55,103 +52,98 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>MechAnime</title>
-        <meta name="description" content="MechAnime" />
+        <title>FL Nurse Honor Guard</title>
+        <meta name="description" content="FL Nurse Honor Guard" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/logo500.png" />
+        <link rel="icon" href="/LOGO.png" />
       </Head>
 
-      <div className="flex flex-col min-h-screen font-robot bg-custom">
-        <Ticker />
-
+      <div className="flex flex-col min-h-screen font-robot bg-gradient-to-b from-blue-200 to-blue-800">
         <div className="flex justify-center items-center">
           <Image
             className="logo"
-            src="/logo.svg"
-            width={80}
-            height={80}
+            src="/LOGO.png"
+            width={120}
+            height={120}
             alt="Logo"
           ></Image>
 
           <h2 className="xl:text-7xl lg:text-5xl md:text-4xl sm:text-3xl xs:text-3xl text-3xl">
-            MechAnime
+            Finger Lakes Nurse Honor Guard
           </h2>
 
           <Image
             className="logo"
-            src="/logo.svg"
-            width={80}
-            height={80}
+            src="/LOGO.png"
+            width={120}
+            height={120}
             alt="Logo"
           ></Image>
         </div>
-
         <div className="p-2 space-y-2 text-xl flex flex-col items-end">
           <button
             className="hover:bg-stone-100 hover:text-[#800000]"
-            onClick={async () => window.open("https://raydium.io/swap/?inputCurrency=sol&outputCurrency=G6KJREZ5TxiLUU6JwYcghbqDa91oU6D6oapM68P6N5Ve&outputSymbol=MECHA&fixed=in")}
+            onClick={async () => window.open("https://donateurl")}
           >
-            Buy MECHA
-          </button>
-
-          <button
-            className="hover:bg-stone-100 hover:text-[#800000]"
-            onClick={async () => window.open("https://dexscreener.com/solana/6b2dr2ylaw6qgq2h15d3w5xhhghh3pgv1vudixhcfryk")}
-          >
-            Chart
+            Donate
           </button>
 
           <div className="flex flex-col ml-auto space-y-4">
-            <SocialIcon
-              url="https://t.me/mech_anime"
-              network="telegram"
-              fgColor="white"
-            />
-            <SocialIcon
-              url="https://twitter.com/mechanime_"
-              network="twitter"
-              fgColor="white"
-            />
+            <SocialIcon url="https:" network="facebook" fgColor="white" />
           </div>
         </div>
-
-        <div className="first mx-auto mb-4 text-center max-w-lg bg-stone-100 bg-opacity-60 md:text-base sm:text-base rounded-lg outline outline-2 outline-slate-800">
-          <p className="m-1 lg:text-2xl md:text-2xl sm:text-xl text-lg">
-            MechAnime pushes the evolution of meme tokens further into art and
-            lore. Inspired by master game artist Akihiko Yoshida, creator of
-            Final Fantasy.
-          </p>
-        </div>
-
-        <div className="flex flex-col justify-center items-center">
-          <h2 className="xl:text-5xl lg:text-4xl md:text-3xl sm:text-3xl xs:text-3xl text-2xl">
-            Contract Address
-          </h2>
-          <div
-            className="p-2 mb-4 max-w-xl hover:outline-white bg-stone-100 bg-opacity-60 md:text-base sm:text-base rounded-lg outline outline-2 outline-slate-800 cursor-pointer"
-            onClick={async () => {
-              try {
-                await navigator.clipboard.writeText(copiedText)
-                setCopiedText("CA Copied!")
-              } catch (err) {
-                console.error("Failed to copy text: ", err)
-              }
-            }}
-          >
-            <p className="lg:text-base text-xs">{copiedText}</p>
-          </div>
-        </div>
-
         <div className="flex justify-center">
           <h2 className="xl:text-5xl lg:text-4xl md:text-3xl sm:text-3xl xs:text-3xl text-2xl">
-            Tokenomics
+            Our Mission
           </h2>
         </div>
-
+        <div className="mx-auto mb-4 text-center max-w-lg bg-stone-100 bg-opacity-60 md:text-base sm:text-base rounded-lg outline outline-2 outline-slate-800">
+          <p className="m-1 lg:text-2xl md:text-2xl sm:text-xl text-lg">
+            The Finger Lakes Nurse Honor Guard is privileged to recognize and
+            honor those who have dedicated their lives to serving others in
+            their chosen profession as a nurse at the end of their life journey.
+            The honor guard’s presence provides meaningful recognition and honor
+            to a nurse who has passed, and in hopes of easing the grief of their
+            family members, loved one’s and colleagues.
+          </p>
+        </div>
+        <div className="flex justify-center">
+          <h2 className="xl:text-5xl lg:text-4xl md:text-3xl sm:text-3xl xs:text-3xl text-2xl">
+            Our Vision
+          </h2>
+        </div>
+        <div className="mx-auto mb-4 text-center max-w-lg bg-stone-100 bg-opacity-60 md:text-base sm:text-base rounded-lg outline outline-2 outline-slate-800">
+          <p className="m-1 lg:text-2xl md:text-2xl sm:text-xl text-lg">
+            Our vision is that no nurse is forgotten. We will provide
+            recognition and honor to every nurse at the end of life upon the
+            family’s request. All cultural and spiritual diversities will be
+            discussed with the Honor Guard and Funeral Director prior to the
+            tribute ceremony. All services will occur totally free of charge.
+          </p>
+        </div>
+        <div className="flex justify-center">
+          <h2 className="xl:text-5xl lg:text-4xl md:text-3xl sm:text-3xl xs:text-3xl text-2xl">
+            Who We Are
+          </h2>
+        </div>
+        <div className="mx-auto mb-4 text-center max-w-lg bg-stone-100 bg-opacity-60 md:text-base sm:text-base rounded-lg outline outline-2 outline-slate-800">
+          <p className="m-1 lg:text-2xl md:text-2xl sm:text-xl text-lg">
+            We are an incredible group of volunteer nurses from the Finger Lakes
+            area, Registered Nurses or Licensed Practical Nurses, Student
+            Nurses, and Nurse Practitioners who pay homage to a nurse who has
+            dedicated their life to serving others in their chosen profession as
+            a nurse at the end of their life journey. It is a heartwarming and
+            truly meaningful way to serve others as we pay honor to our
+            colleagues at the time of their passing.
+          </p>
+        </div>
         <div className="flex flex-row justify-center mb-4 space-x-3">
           <div className="p-2 text-center bg-stone-100 bg-opacity-60 md:text-base sm:text-base rounded-lg outline outline-2 outline-slate-800 w-54">
             <p className="lg:text-2xl md:text-2xl sm:text-xl xs:text-lg text-md">
-              <a className="hover:underline" href="https://solscan.io/tx/2ccG1HMedF1XrP5qkSjEdaE4adSutSWwEpBUYqcYxHMzdiUedsuskFYcx1zAQG6SxCre8bRkaR3D5u3xSxBgHJF5">
+              <a
+                className="hover:underline"
+                href="https://solscan.io/tx/2ccG1HMedF1XrP5qkSjEdaE4adSutSWwEpBUYqcYxHMzdiUedsuskFYcx1zAQG6SxCre8bRkaR3D5u3xSxBgHJF5"
+              >
                 LP Burnt
               </a>
             </p>
@@ -206,42 +198,23 @@ export default function Home() {
             </p>
           </div>
         </div>
-
         <div className="flex justify-center">
           <h2 className="xl:text-5xl lg:text-4xl md:text-3xl sm:text-3xl xs:text-3xl text-2xl">
-            Launched
+            Highlights
           </h2>
         </div>
-
         <div className="mx-auto mb-4">
           <Image
             className="rounded-lg"
-            src="launch.png"
+            src="/images/conference.jpg"
             width={840}
             height={300}
             alt="Code"
           ></Image>
         </div>
-
-        <div className="flex justify-center">
+        <div className="flex flex-col justify-center items-center">
           <h2 className="xl:text-5xl lg:text-4xl md:text-3xl sm:text-3xl xs:text-3xl text-2xl">
-            Backstory
-          </h2>
-        </div>
-
-        <div className="mx-auto mb-4 text-center max-w-lg bg-stone-100 bg-opacity-60 md:text-base sm:text-base rounded-lg outline outline-2 outline-slate-800">
-          <p className="m-1 lg:text-2xl md:text-2xl sm:text-xl text-lg">
-            As holy wars escalated into nuclear apocalypse, the world's elite
-            withdrew into fortified underground vaults. They took the best AI,
-            robotics, and weapons tech with them. Decades later their
-            mecha-warrior daughters emerge into the dune wasteland to battle for
-            the planet's last resources. Follow their story on X and TG.
-          </p>
-        </div>
-
-        <div className="flex flex-col justify-center items-center bg-[#dfccb7]">
-          <h2 className="xl:text-5xl lg:text-4xl md:text-3xl sm:text-3xl xs:text-3xl text-2xl">
-            Characters
+            Memory Wall
           </h2>
           <SlideshowLightbox
             className="container grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-3 mx-auto"
@@ -284,7 +257,10 @@ export default function Home() {
               alt="Logo"
             ></Image>
             <p className="ticker m-2 lg:text-3xl md:text-2xl sm:text-2xl text-2xl text-white">
-              <a className="hover:underline" href="https://raydium.io/swap/?inputCurrency=sol&outputCurrency=G6KJREZ5TxiLUU6JwYcghbqDa91oU6D6oapM68P6N5Ve&outputSymbol=MECHA&fixed=in">
+              <a
+                className="hover:underline"
+                href="https://raydium.io/swap/?inputCurrency=sol&outputCurrency=G6KJREZ5TxiLUU6JwYcghbqDa91oU6D6oapM68P6N5Ve&outputSymbol=MECHA&fixed=in"
+              >
                 Ticker : $MECHA
               </a>
             </p>
@@ -296,7 +272,6 @@ export default function Home() {
               alt="Logo"
             ></Image>
           </div>
-          
         </div>
       </div>
     </>
